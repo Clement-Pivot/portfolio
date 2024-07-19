55795918555134d6b3bd5017f9038e0057e7525f
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-07-19 10:24:51 +0200

    feat: hover on project card ok

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -11,26 +11,35 @@ type Props = {
 
 export function ProjectCard({ href, gif, picture, title, description }: Props) {
   const dialog_ref = useRef(null)
-  function showModal() {}
+  const showModal = () => dialog_ref!.current!.showModal()
+  const hideModal = () => dialog_ref!.current!.close()
   return (
-    <div className="project-card">
-      <img
-        src={picture}
-        alt={`Screenshot miniature du projet ${title}`}
-        className="project-card__hero"
-      />
-      <img
-        src={gif}
-        alt={`Clip miniature du projet ${title}`}
-        className="project-card__moving"
-        onClick={showModal}
-      />
+    <>
+      <div className="project-card">
+        <img
+          src={picture}
+          alt={`Screenshot miniature du projet ${title}`}
+          className="project-card__hero"
+        />
+        <img
+          src={gif}
+          alt={`Clip miniature du projet ${title}`}
+          className="project-card__moving"
+          onClick={showModal}
+        />
+      </div>
       <dialog className="project-card__dialog" ref={dialog_ref}>
-        <img src={gif} alt={`Clip du projet ${title}`} />
+        <img
+          src={gif}
+          alt={`Clip du projet ${title}`}
+          className="project-card__show"
+        />
         <p className="project-card__title">{title}</p>
         <p className="project-card__description">{description}</p>
+        <a href={href} className="project-card__link">
+          Lien vers la page github
+        </a>
       </dialog>
-      <a href={href} className="project-card__link"></a>
-    </div>
+    </>
   )
 }
